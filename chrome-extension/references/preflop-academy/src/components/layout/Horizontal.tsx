73ad9d77d685c0cca2698bfcae653506b65c4bb7
Michael Vuolo <53955079+vuolo@users.noreply.github.com>
2023-09-19 15:18:37 -0400

    Delete chrome-extension/references directory

--- chrome-extension/references/preflop-academy/src/components/layout/Horizontal.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Horizontal = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex: 1;
-`
-
-export default Horizontal
--- chrome-extension/references/preflop-academy/src/components/layout/Vertical.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-const Vertical = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-`
-export default Vertical
